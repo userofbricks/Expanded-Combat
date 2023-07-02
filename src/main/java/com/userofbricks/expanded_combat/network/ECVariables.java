350c9ea373df22dc6566f37eabe13593d0fd0cab
userofbricks <userofbricks11@gmail.com>
2023-07-02 10:04:51 -0600

    almost missed ECVariables.java

--- src/main/java/com/userofbricks/expanded_combat/network/ECVariables.java
+++ src/main/java/com/userofbricks/expanded_combat/network/ECVariables.java
@@ -60,19 +60,19 @@ public class ECVariables {
     public static class EventBusVariableHandlers {
         @SubscribeEvent
         public static void onPlayerLoggedInSyncPlayerVariables(PlayerEvent.PlayerLoggedInEvent event) {
-            if (!event.getEntity().level.isClientSide())
+            if (!event.getEntity().level().isClientSide())
                 event.getEntity().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()).syncPlayerVariables(event.getEntity());
         }
 
         @SubscribeEvent
         public static void onPlayerRespawnedSyncPlayerVariables(PlayerEvent.PlayerRespawnEvent event) {
-            if (!event.getEntity().level.isClientSide())
+            if (!event.getEntity().level().isClientSide())
                 event.getEntity().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()).syncPlayerVariables(event.getEntity());
         }
 
         @SubscribeEvent
         public static void onPlayerChangedDimensionSyncPlayerVariables(PlayerEvent.PlayerChangedDimensionEvent event) {
-            if (!event.getEntity().level.isClientSide())
+            if (!event.getEntity().level().isClientSide())
                 event.getEntity().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()).syncPlayerVariables(event.getEntity());
         }
 
