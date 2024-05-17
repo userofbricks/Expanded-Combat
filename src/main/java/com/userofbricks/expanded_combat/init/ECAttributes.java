20ae3f6a4d7d0932e6b1297854aa4b4a5533601f
userofbricks <userofbricks11@gmail.com>
2024-05-17 12:00:18 -0600

    updated attribute init class

--- src/main/java/com/userofbricks/expanded_combat/init/ECAttributes.java
+++ src/main/java/com/userofbricks/expanded_combat/init/ECAttributes.java
@@ -2,19 +2,19 @@ package com.userofbricks.expanded_combat.init;
 
 import com.userofbricks.expanded_combat.ExpandedCombat;
 import com.userofbricks.expanded_combat.entity.attributes.RandedDamageAttribute;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 import static com.userofbricks.expanded_combat.ExpandedCombat.modLoc;
 
 public class ECAttributes {
-    public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, ExpandedCombat.MODID);
+    public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(BuiltInRegistries.ATTRIBUTE, ExpandedCombat.MODID);
 
-    public static final RegistryObject<RandedDamageAttribute> GAUNTLET_DMG_WITHOUT_WEAPON = ATTRIBUTES.register("dmg_no_weapon", () -> new RandedDamageAttribute(modLoc("dmg_no_weapon"), 0, 2048));
-    public static final RegistryObject<RandedDamageAttribute> HEAT_DMG = ATTRIBUTES.register("heat_dmg", () -> new RandedDamageAttribute(modLoc("heat_dmg"), 0, 2048));
-    public static final RegistryObject<RandedDamageAttribute> COLD_DMG = ATTRIBUTES.register("cold_dmg", () -> new RandedDamageAttribute(modLoc("cold_dmg"), 0, 2048));
-    public static final RegistryObject<RandedDamageAttribute> VOID_DMG = ATTRIBUTES.register("void_dmg", () -> new RandedDamageAttribute(modLoc("void_dmg"), 0, 2048));
-    public static final RegistryObject<RandedDamageAttribute> SOUL_DMG = ATTRIBUTES.register("soul_dmg", () -> new RandedDamageAttribute(modLoc("soul_dmg"), 0, 2048));
+    public static final DeferredHolder<Attribute, RandedDamageAttribute> GAUNTLET_DMG_WITHOUT_WEAPON = ATTRIBUTES.register("dmg_no_weapon", () -> new RandedDamageAttribute(modLoc("dmg_no_weapon"), 0, 2048));
+    public static final DeferredHolder<Attribute, RandedDamageAttribute> HEAT_DMG = ATTRIBUTES.register("heat_dmg", () -> new RandedDamageAttribute(modLoc("heat_dmg"), 0, 2048));
+    public static final DeferredHolder<Attribute, RandedDamageAttribute> COLD_DMG = ATTRIBUTES.register("cold_dmg", () -> new RandedDamageAttribute(modLoc("cold_dmg"), 0, 2048));
+    public static final DeferredHolder<Attribute, RandedDamageAttribute> VOID_DMG = ATTRIBUTES.register("void_dmg", () -> new RandedDamageAttribute(modLoc("void_dmg"), 0, 2048));
+    public static final DeferredHolder<Attribute, RandedDamageAttribute> SOUL_DMG = ATTRIBUTES.register("soul_dmg", () -> new RandedDamageAttribute(modLoc("soul_dmg"), 0, 2048));
 }
