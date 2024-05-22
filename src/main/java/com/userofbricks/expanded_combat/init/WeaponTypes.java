d08badf55c6f75d02a6720a50732c5a210790177
userofbricks <userofbricks11@gmail.com>
2024-05-22 17:33:33 -0600

    finished moving weapon materials to weapon type datagen

--- src/main/java/com/userofbricks/expanded_combat/init/WeaponTypes.java
+++ src/main/java/com/userofbricks/expanded_combat/init/WeaponTypes.java
@@ -2,6 +2,7 @@ package com.userofbricks.expanded_combat.init;
 
 import com.userofbricks.expanded_combat.data.material.Material;
 import com.userofbricks.expanded_combat.data.material.PlacementInShield;
+import com.userofbricks.expanded_combat.data.weapon_type.GripType;
 import com.userofbricks.expanded_combat.data.weapon_type.WeaponType;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistrySetBuilder;
@@ -11,6 +12,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import org.lwjgl.opengl.GL;
 
 import java.util.List;
 import java.util.Optional;
@@ -51,8 +53,47 @@ public class WeaponTypes {
     public static final RegistrySetBuilder registrySetBuilder = new RegistrySetBuilder()
             .add(Registries.WEAPON_TYPE_REGISTRY_KEY, bootstrap -> {
                 BATTLE_STAFF = bootstrap.register(BATTLE_STAFF_KEY,
-                        new WeaponType(false, )
-                        );
+                        new WeaponType(false, 0.9, -2, -1.4f, 0.1f, 1, 1.5, GripType.TWOHANDED)
+                );
+                BROAD_SWORD = bootstrap.register(BROAD_SWORD_KEY,
+                        new WeaponType(false, 1.1, 3, -3f, 0f, 0, 0.5, GripType.TWOHANDED)
+                );
+                CLAYMORE = bootstrap.register(CLAYMORE_KEY,
+                        new WeaponType(false, 1.1, 2, -3f, 0f, 0, 1, GripType.TWOHANDED)
+                );
+                CUTLASS = bootstrap.register(CUTLASS_KEY,
+                        new WeaponType(false, 1, 0, -2.2f, 0.2f, 0, 0, GripType.ONEHANDED)
+                );
+                DAGGER = bootstrap.register(DAGGER_KEY,
+                        new WeaponType(false, 0.75, -1, -1.2f, 0.1f, 0, 0, GripType.DUALWIELD)
+                );
+                DANCERS_SWORD = bootstrap.register(DANCERS_SWORD_KEY,
+                        new WeaponType(false, 1.3, 2, -1.8f, 0.2f, 0, 0, GripType.ONEHANDED)
+                );
+                FLAIL = bootstrap.register(FLAIL_KEY,
+                        new WeaponType(false, 1.1, 4, -3.4f, 0f, 0.5f, 1, GripType.ONEHANDED)
+                );
+                GLAIVE = bootstrap.register(GLAIVE_KEY,
+                        new WeaponType(false, 1, 3, -3.2f, 0.1f, 0.5f, 2, GripType.TWOHANDED)
+                );
+                GREAT_HAMMER = bootstrap.register(GREAT_HAMMER_KEY,
+                        new WeaponType(false, 1.5, 5, -3.3f, 0f, 1, 0, GripType.TWOHANDED)
+                );
+                KATANA = bootstrap.register(KATANA_KEY,
+                        new WeaponType(false, 1, 2, -2.4f, 0f, 0, 0.5, GripType.ONEHANDED)
+                );
+                MACE = bootstrap.register(MACE_KEY,
+                        new WeaponType(false, 1.1, 4, -3.2f, 0f, 0.5f, 0, GripType.ONEHANDED)
+                );
+                SCYTHE = bootstrap.register(SCYTHE_KEY,
+                        new WeaponType(false, 1.2, 4, -3.4f, 0.1f, 1f, 2, GripType.TWOHANDED)
+                );
+                SICKLE = bootstrap.register(SICKLE_KEY,
+                        new WeaponType(false, 0.8, 0, -1.8f, 0.2f, 0f, 0, GripType.DUALWIELD)
+                );
+                SPEAR = bootstrap.register(SPEAR_KEY,
+                        new WeaponType(false, 1, 3, -3.4f, 0.1f, 0.5f, 2, GripType.TWOHANDED)
+                );
             });
     public static ResourceKey<WeaponType> createWeaponTypeKey(ResourceLocation id) {
         return ResourceKey.create(
