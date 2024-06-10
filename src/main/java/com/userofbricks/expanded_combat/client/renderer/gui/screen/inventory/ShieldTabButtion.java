257656560395b35553e1fb3fd16b9641eaf17502
userofbricks <userofbricks11@gmail.com>
2024-06-10 00:04:47 -0600

    updated shield item renderer to use vanilla shield model

--- src/main/java/com/userofbricks/expanded_combat/client/renderer/gui/screen/inventory/ShieldTabButtion.java
+++ src/main/java/com/userofbricks/expanded_combat/client/renderer/gui/screen/inventory/ShieldTabButtion.java
@@ -8,7 +8,6 @@ import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.network.PacketDistributor;
 
 public class ShieldTabButtion extends ImageButton {
 
--- src/main/java/com/userofbricks/expanded_combat/client/renderer/item/ECShieldBlockEntityWithoutLevelRenderer.java
+++ src/main/java/com/userofbricks/expanded_combat/client/renderer/item/ECShieldBlockEntityWithoutLevelRenderer.java
@@ -1,106 +1,111 @@
 package com.userofbricks.expanded_combat.client.renderer.item;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.datafixers.util.Pair;
-import com.mojang.math.Axis;
-import com.userofbricks.expanded_combat.init.ECLayerDefinitions;
-import com.userofbricks.expanded_combat.client.model.ECBaseShieldModel;
-import com.userofbricks.expanded_combat.client.model.ECShieldBanner;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.userofbricks.expanded_combat.data_components.ShieldMaterials;
+import com.userofbricks.expanded_combat.init.ItemDataComponents;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ShieldModel;
 import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.blockentity.BannerRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.armortrim.ArmorTrim;
-import net.minecraft.world.level.block.entity.BannerBlockEntity;
-import net.minecraft.world.level.block.entity.BannerPattern;
+import net.minecraft.world.level.block.entity.BannerPatternLayers;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
-import java.util.List;
-
-import static com.userofbricks.expanded_combat.init.PluginInit.valueOfShield;
+import java.util.Objects;
 
 @ParametersAreNonnullByDefault
 public class ECShieldBlockEntityWithoutLevelRenderer extends BlockEntityWithoutLevelRenderer {
+    private final ShieldModel shieldModel;
 
     public ECShieldBlockEntityWithoutLevelRenderer() {
-        super(Minecraft.getInstance().getBlockEntityRenderDispatcher(), new EntityModelSet());
+        this(Minecraft.getInstance().getBlockEntityRenderDispatcher(), new EntityModelSet());
+    }
+
+    public ECShieldBlockEntityWithoutLevelRenderer(BlockEntityRenderDispatcher blockEntityRenderDispatcher, EntityModelSet entityModelSet) {
+        super(blockEntityRenderDispatcher, entityModelSet);
+        this.shieldModel = new ShieldModel(entityModelSet.bakeLayer(ModelLayers.SHIELD));
     }
 
     public void renderByItem(ItemStack stack, ItemDisplayContext transformType, PoseStack poseStack, MultiBufferSource multiBufferSource, int combinedLight, int combinedOverlay) {
-        if(!stack.isEmpty()) {
-            Minecraft mc = Minecraft.getInstance();
-            if(mc.player != null) {
-                String sul = stack.getOrCreateTag().getString("UL_Material");
-                String sur = stack.getOrCreateTag().getString("UR_Material");
-                String sdl = stack.getOrCreateTag().getString("DL_Material");
-                String sdr = stack.getOrCreateTag().getString("DR_Material");
-                String sm =  stack.getOrCreateTag().getString("M_Material" );
-                assert mc.level != null;
-                String trimName = "empty";
-                ArmorTrim trim = ArmorTrim.getTrim(mc.level.registryAccess(), stack).orElse(null);
-                if (trim != null) {
-                    ResourceLocation trimResourceLocation = trim.pattern().get().assetId();
-                    trimName = trimResourceLocation.getNamespace() + "__" + trimResourceLocation.getPath();
-                }
-                ResourceLocation rlUL = valueOfShield("ul", sul).getLocationName().withPrefix("model/shields/" + trimName + "/ul/");
-                ResourceLocation rlUR = valueOfShield("ur", sur).getLocationName().withPrefix("model/shields/" + trimName + "/ur/");
-                ResourceLocation rlDL = valueOfShield("dl", sdl).getLocationName().withPrefix("model/shields/" + trimName + "/dl/");
-                ResourceLocation rlDR = valueOfShield("dr", sdr).getLocationName().withPrefix("model/shields/" + trimName + "/dr/");
-                ResourceLocation rlM = valueOfShield("m", sm).getLocationName().withPrefix("model/shields/m/");
+        if (!stack.isEmpty()) {
+            BannerPatternLayers bannerpatternlayers = stack.getOrDefault(DataComponents.BANNER_PATTERNS, BannerPatternLayers.EMPTY);
+            DyeColor dyecolor = stack.get(DataComponents.BASE_COLOR);
+            boolean hasBanner = !bannerpatternlayers.layers().isEmpty() || dyecolor != null;
+            poseStack.pushPose();
+            poseStack.scale(1.0F, -1.0F, -1.0F);
 
-                ECBaseShieldModel upperLeft = new ECBaseShieldModel(Minecraft.getInstance().getEntityModels().bakeLayer(ECLayerDefinitions.SHIELD_UPPER_LEFT));
-                ECBaseShieldModel upperRight = new ECBaseShieldModel(Minecraft.getInstance().getEntityModels().bakeLayer(ECLayerDefinitions.SHIELD_UPPER_RIGHT));
-                ECBaseShieldModel lowerLeft = new ECBaseShieldModel(Minecraft.getInstance().getEntityModels().bakeLayer(ECLayerDefinitions.SHIELD_LOWER_LEFT));
-                ECBaseShieldModel lowerRight = new ECBaseShieldModel(Minecraft.getInstance().getEntityModels().bakeLayer(ECLayerDefinitions.SHIELD_LOWER_RIGHT));
-                ECBaseShieldModel middle = new ECBaseShieldModel(Minecraft.getInstance().getEntityModels().bakeLayer(ECLayerDefinitions.SHIELD_MIDDLE));
-
-                poseStack.pushPose();
-                poseStack.translate(0, -1f - ((1f/16f)*5f), 1f / 16f);
-                poseStack.rotateAround(Axis.ZP.rotationDegrees(180), 0.0F, 1f + ((1f/16f)*5f), -1f / 16f);
-                if (stack.getItem() instanceof DyeableLeatherItem dyeableLeatherItem) {
-                    int i = dyeableLeatherItem.getColor(stack);
-                    float f = (float)(i >> 16 & 255) / 255.0F;
-                    float f1 = (float)(i >> 8 & 255) / 255.0F;
-                    float f2 = (float)(i & 255) / 255.0F;
-                    renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), upperLeft, f, f1, f2, rlUL);
-                    renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), upperRight, f, f1, f2, rlUR);
-                    renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), lowerLeft, f, f1, f2, rlDL);
-                    renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), lowerRight, f, f1, f2, rlDR);
-                }
-                else {
-                    renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), upperLeft, rlUL);
-                    renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), upperRight, rlUR);
-                    renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), lowerLeft, rlDL);
-                    renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), lowerRight, rlDR);
-                }
-                renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), middle, rlM);
-
-                if (BlockItem.getBlockEntityData(stack) != null) {
-                    ECShieldBanner shieldModel = new ECShieldBanner(mc.getEntityModels().bakeLayer(ECLayerDefinitions.SHIELD_BANNER));
-                    Material material = new Material(Sheets.SHIELD_SHEET, new ResourceLocation("expanded_combat", "model/shields/shield_base"));
-                    List<Pair<Holder<BannerPattern>, DyeColor>> list = BannerBlockEntity.createPatterns(ShieldItem.getColor(stack), BannerBlockEntity.getItemPatterns(stack));
-                    BannerRenderer.renderPatterns(poseStack, multiBufferSource, combinedLight, combinedOverlay, shieldModel.banner(), material, false, list, stack.hasFoil());
-                }
-                poseStack.popPose();
+            ShieldMaterials shieldMaterials = stack.getOrDefault(ItemDataComponents.SHIELD_MATERIALS, ShieldMaterials.DEFAULT);
+            String trimName = "empty";
+            ArmorTrim trim = stack.get(DataComponents.TRIM);
+            if (trim != null) {
+                ResourceLocation trimResourceLocation = trim.pattern().value().assetId();
+                trimName = trimResourceLocation.getNamespace() + "__" + trimResourceLocation.getPath();
             }
+            ResourceLocation rlUL = getShieldTexture(shieldMaterials.ULMaterial, trimName, "ul");
+            ResourceLocation rlUR = getShieldTexture(shieldMaterials.URMaterial, trimName, "ur");
+            ResourceLocation rlDL = getShieldTexture(shieldMaterials.DLMaterial, trimName, "dl");
+            ResourceLocation rlDR = getShieldTexture(shieldMaterials.DRMaterial, trimName, "dr");
+            ResourceLocation rlM = getShieldTexture(shieldMaterials.MMaterial, trimName, "m");
+
+            renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), this.shieldModel.handle(), rlM);
+
+            renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), this.shieldModel.plate(), rlUL);
+            renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), this.shieldModel.plate(), rlUR);
+            renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), this.shieldModel.plate(), rlDL);
+            renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), this.shieldModel.plate(), rlDR);
+            renderModel(poseStack, multiBufferSource, combinedLight, stack.hasFoil(), this.shieldModel.plate(), rlM);
+
+            if (hasBanner) {
+                Material material = new Material(Sheets.SHIELD_SHEET, new ResourceLocation("expanded_combat", "model/shields/shield_base"));
+                BannerRenderer.renderPatterns(
+                        poseStack,
+                        multiBufferSource,
+                        combinedLight,
+                        combinedOverlay,
+                        this.shieldModel.plate(),
+                        material,
+                        false,
+                        Objects.requireNonNullElse(dyecolor, DyeColor.WHITE),
+                        bannerpatternlayers,
+                        stack.hasFoil()
+                );
+            }
+            poseStack.popPose();
         }
     }
 
-    private void renderModel(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, boolean foil, ECBaseShieldModel model, ResourceLocation shieldResource) {
-        renderModel(poseStack, multiBufferSource, light, foil, model, 1f, 1f, 1f, shieldResource);
+    private ResourceLocation getShieldTexture(Holder<com.userofbricks.expanded_combat.data.material.Material> materialHolder, String trimName, String p) {
+        ResourceLocation material = new ResourceLocation(materialHolder.getRegisteredName());
+        return p.equals("m") ? material.withPrefix("model/shields/m/") : material.withPrefix("model/shields/" + trimName + "/" + p + "/");
     }
 
-    private void renderModel(PoseStack poseStack, MultiBufferSource multibuffersource, int light, boolean foil, ECBaseShieldModel model, float f, float f1, float f2, ResourceLocation shieldResource) {
-        Material material = new Material(Sheets.SHIELD_SHEET, shieldResource);
-        model.renderToBuffer(poseStack, material.buffer(multibuffersource, RenderType::armorCutoutNoCull, foil), light, OverlayTexture.NO_OVERLAY, f, f1, f2, 1.0F);
+    private void renderModel(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, boolean foil, ModelPart model, ResourceLocation... shieldResources) {
+        renderModel(poseStack, multiBufferSource, light, foil, model, 1f, 1f, 1f, shieldResources);
+    }
+
+    private void renderModel(PoseStack poseStack, MultiBufferSource multibuffersource, int light, boolean foil, ModelPart model, float red, float green, float blue, ResourceLocation... shieldResources) {
+        for (ResourceLocation shieldResource : shieldResources) {
+            Material material = new Material(Sheets.SHIELD_SHEET, shieldResource);
+            VertexConsumer vertexconsumer = material.sprite()
+                    .wrap(ItemRenderer.getFoilBufferDirect(multibuffersource, RenderType.armorCutoutNoCull(material.atlasLocation()), true, foil));
+            model.render(poseStack, vertexconsumer, light, OverlayTexture.NO_OVERLAY, red, green, blue, 1.0F);
+        }
     }
 }
