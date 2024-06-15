f348a2265270a16cd402c5d8d01094daf702dc4d
userofbricks <userofbricks11@gmail.com>
2024-06-14 18:28:49 -0600

    updated enchantment events

--- src/main/java/com/userofbricks/expanded_combat/events/EnchantentEvents.java
+++ src/main/java/com/userofbricks/expanded_combat/events/EnchantentEvents.java
@@ -11,16 +11,20 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.common.NeoForgeMod;
 import net.neoforged.neoforge.event.ItemAttributeModifierEvent;
 import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotResult;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public class EnchantentEvents {
@@ -45,8 +49,8 @@ public class EnchantentEvents {
         if (stack.getEnchantmentLevel(ECEnchantments.AGILITY.get()) > 0 && Mob.getEquipmentSlotForItem(stack) == equipmentSlot) {
             int level = stack.getEnchantmentLevel(ECEnchantments.AGILITY.get());
             switch (equipmentSlot) {
-                case FEET -> event.addModifier(Attributes.MOVEMENT_SPEED, new AttributeModifier(UUID.fromString("33dad864-864b-4dbd-acae-88b72cc358cf"), "Movement Speed", (level * 0.2), AttributeModifier.Operation.MULTIPLY_BASE));
-                case LEGS -> event.addModifier(ForgeMod.SWIM_SPEED.get(), new AttributeModifier(UUID.fromString("33dad864-864b-4dbd-acae-88b72cc358cf"), "Jump Strength", (level * 0.2), AttributeModifier.Operation.MULTIPLY_BASE));
+                case FEET -> event.addModifier(Attributes.MOVEMENT_SPEED, new AttributeModifier(UUID.fromString("33dad864-864b-4dbd-acae-88b72cc358cf"), "Movement Speed", (level * 0.2), AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
+                case LEGS -> event.addModifier(NeoForgeMod.SWIM_SPEED, new AttributeModifier(UUID.fromString("33dad864-864b-4dbd-acae-88b72cc358cf"), "Jump Strength", (level * 0.2), AttributeModifier.Operation.ADD_MULTIPLIED_BASE));
                 default -> {}
             }
         }
@@ -54,11 +58,14 @@ public class EnchantentEvents {
 
     @SubscribeEvent
     public static void miningSpeed(PlayerEvent.BreakSpeed event) {
-        float speedIncrease = 0;
-        for (SlotResult slotResult : CuriosApi.getCuriosHelper().findCurios(event.getEntity(), stack -> stack.getItem() instanceof GauntletItem)) {
-            speedIncrease += slotResult.stack().getEnchantmentLevel(ECEnchantments.AGILITY.get()) * 0.2f;
+        Optional<ICuriosItemHandler> handlerOptional = CuriosApi.getCuriosInventory(event.getEntity());
+        if (handlerOptional.isPresent()) {
+            float speedIncrease = 0;
+            for (SlotResult slotResult : handlerOptional.get().findCurios(stack -> stack.getItem() instanceof GauntletItem)) {
+                speedIncrease += slotResult.stack().getEnchantmentLevel(ECEnchantments.AGILITY.get()) * 0.2f;
+            }
+            event.setNewSpeed(event.getOriginalSpeed() - speedIncrease);
         }
-        event.setNewSpeed(event.getOriginalSpeed() - speedIncrease);
     }
 
     @SubscribeEvent
@@ -68,11 +75,10 @@ public class EnchantentEvents {
         int agility = chestplate.getEnchantmentLevel(ECEnchantments.AGILITY.get());
         if (!chestplate.isEmpty() && agility > 0 && entity.getRandom().nextIntBetweenInclusive(1, Math.round((20 + agility)-(10 * ((float)Math.sqrt(agility)-1)))) == 1) {
             //TODO: movement when
-            float move = 0.5f / agility;
+            float move = 1f / agility;
             double xMove = entity.getRandom().nextInt(0, 1) == 1 ? -1 : 1;
             double yMove = entity.getRandom().nextInt(0, 1) == 1 ? -1 : 1;
-            //entity.push(entity.getRandom().nextInt(0, 1) == 1 ? xMove  : 0, 0.1, entity.getRandom().nextInt(0, 1) == 1 ? yMove  : 0);
-            entity.moveRelative(move, entity.position().add(xMove, 0.5d, yMove));
+            entity.addDeltaMovement(new Vec3(xMove * move, 0.5d, yMove * move));
             event.setCanceled(true);
         }
     }
--- src/main/java/com/userofbricks/expanded_combat/events/GauntletEvents.java
+++ src/main/java/com/userofbricks/expanded_combat/events/GauntletEvents.java
@@ -1,38 +1,29 @@
 package com.userofbricks.expanded_combat.events;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import com.userofbricks.expanded_combat.ExpandedCombat;
 import com.userofbricks.expanded_combat.api.client.IGauntletRenderer;
-import com.userofbricks.expanded_combat.item.GauntletItem;
+import com.userofbricks.expanded_combat.init.ECItems;
 import com.userofbricks.expanded_combat.item.ECQuiverItem;
-import com.userofbricks.expanded_combat.plugins.CustomWeaponsPlugin;
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.MultiBufferSource;
+import com.userofbricks.expanded_combat.item.GauntletItem;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RenderArmEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.Cancelable;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.LogicalSide;
-import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.ApiStatus;
+import net.minecraft.world.item.component.BundleContents;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderArmEvent;
+import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
+import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.SlotResult;
-import top.theillusivec4.curios.api.SlotTypePreset;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
@@ -41,33 +32,40 @@ import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
 import java.util.List;
 import java.util.Optional;
 
-@Mod.EventBusSubscriber(modid = "expanded_combat", bus = Mod.EventBusSubscriber.Bus.FORGE)
+import static com.userofbricks.expanded_combat.init.ItemDataComponents.CHARGE;
+import static net.minecraft.core.component.DataComponents.BUNDLE_CONTENTS;
+
+@EventBusSubscriber(modid = "expanded_combat", bus = EventBusSubscriber.Bus.GAME)
 public class GauntletEvents
 {
     public static void DamageGauntletEvent(AttackEntityEvent event) {
         Player player = event.getEntity();
         if (player.isCreative()) return;
-        List<SlotResult> slotResults = CuriosApi.getCuriosHelper().findCurios(player, itemStack -> itemStack.getItem() instanceof GauntletItem);
-        if (slotResults.isEmpty()) return;
-        for (SlotResult slotResult : slotResults) {
-            ItemStack stack = slotResult.stack();
-            SlotContext slotContext = slotResult.slotContext();
-            if (stack.getItem() instanceof GauntletItem) {
-                stack.hurtAndBreak(1, (LivingEntity) player, damager -> CuriosApi.getCuriosHelper().onBrokenCurio(slotContext));
+        Optional<ICuriosItemHandler> handlerOptional = CuriosApi.getCuriosInventory(event.getEntity());
+        if (handlerOptional.isPresent()) {
+            List<SlotResult> slotResults = handlerOptional.get().findCurios(itemStack -> itemStack.getItem() instanceof GauntletItem);
+            if (slotResults.isEmpty()) return;
+            for (SlotResult slotResult : slotResults) {
+                ItemStack stack = slotResult.stack();
+                SlotContext slotContext = slotResult.slotContext();
+                if (stack.getItem() instanceof GauntletItem) {
+                    stack.hurtAndBreak(1, player.getRandom(), player, () -> CuriosApi.broadcastCurioBreakEvent(slotContext));
+                }
             }
         }
     }
     @SubscribeEvent
     public static void moreDamageSources(LivingAttackEvent ev) {
         LivingEntity target = ev.getEntity();
-        Optional<SlotResult> optionalSlotResult = CuriosApi.getCuriosHelper().findFirstCurio(target, CustomWeaponsPlugin.MAULERS.getGauntletEntry().get());
+        Optional<SlotResult> optionalSlotResult = CuriosApi.getCuriosInventory(target).flatMap(curiosInventory -> curiosInventory.findFirstCurio(ECItems.BERSERK_GAUNTLETS.get()));
         if (optionalSlotResult.isPresent()) {
             SlotResult slotResult = optionalSlotResult.get();
-            int charge = slotResult.stack().getOrCreateTag().getInt("charge");
+            int charge = slotResult.stack().getOrDefault(CHARGE, 0);
             if (charge >= 20) {
                 target.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 5*20, 2));
+                slotResult.stack().set(CHARGE, charge - 20);
             } else {
-                slotResult.stack().getOrCreateTag().putInt("charge", charge + 1);
+                slotResult.stack().set(CHARGE, charge + 1);
             }
         }
     }
@@ -75,22 +73,24 @@ public class GauntletEvents
     @SubscribeEvent
     public static void pulOutArrow(PlayerInteractEvent.RightClickEmpty event) {
         Player player = event.getEntity();
-        Optional<SlotResult> optionalSlotResult = CuriosApi.getCuriosHelper().findFirstCurio(player, CustomWeaponsPlugin.FIGHTER.getGauntletEntry().get());
+        Optional<ICuriosItemHandler> handlerOptional = CuriosApi.getCuriosInventory(player);
+        Optional<SlotResult> optionalSlotResult = handlerOptional.flatMap(curiosInventory -> curiosInventory.findFirstCurio(ECItems.FIGHTERS_GAUNTLETS.get()));
         if (optionalSlotResult.isPresent() && player.getArrowCount() >= 1) {
-            LazyOptional<ICuriosItemHandler> lazyOptional = CuriosApi.getCuriosHelper().getCuriosHandler(player);
-            Optional<SlotResult> optionalQuiverSlotResult = CuriosApi.getCuriosHelper().findFirstCurio(player, stack -> stack.getItem() instanceof ECQuiverItem);
-            if (lazyOptional.resolve().isPresent() && optionalQuiverSlotResult.isPresent()) {
-                ECQuiverItem quiverItem = (ECQuiverItem) optionalQuiverSlotResult.get().stack().getItem();
-                IDynamicStackHandler arrowStackHandler = lazyOptional.resolve().get().getCurios().get(ARROWS_CURIOS_IDENTIFIER).getStacks();
-                int slots = arrowStackHandler.getSlots();
-                boolean found = false;
-                for (int s = 0; s < slots; s++) {
-                    ItemStack currentStack = arrowStackHandler.getStackInSlot(s);
-                    if ((currentStack.getItem() == Items.ARROW || currentStack.isEmpty()) && quiverItem.providedSlots > s) {
-                        found = arrowStackHandler.insertItem(s, new ItemStack(Items.ARROW), false).isEmpty();
-                    }
-                }
-                if (!found) {
+            Optional<SlotResult> optionalQuiverSlotResult = handlerOptional.get().findFirstCurio(stack -> stack.getItem() instanceof ECQuiverItem);
+            if (optionalQuiverSlotResult.isPresent()) {
+                ItemStack quiverStack = optionalQuiverSlotResult.get().stack();
+                ECQuiverItem quiverItem = (ECQuiverItem) quiverStack.getItem();
+
+                BundleContents bundlecontents = quiverStack.getOrDefault(BUNDLE_CONTENTS, BundleContents.EMPTY);
+
+                ECQuiverItem.MutableQuiverContents bundlecontents$mutable = new ECQuiverItem.MutableQuiverContents(bundlecontents, quiverItem.getMaterial().offense().quiverSlots());
+
+                int i = bundlecontents$mutable.tryInsert(new ItemStack(Items.ARROW));
+                if (i > 0) {
+                    quiverStack.set(BUNDLE_CONTENTS, bundlecontents$mutable.toImmutable());
+                    quiverItem.playInsert(player);
+                    player.awardStat(Stats.ITEM_PICKED_UP.get(Items.ARROW), i);
+                } else {
                     player.getInventory().placeItemBackInInventory(new ItemStack(Items.ARROW));
                 }
             } else {
@@ -102,20 +102,17 @@ public class GauntletEvents
 
     @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
-    @SuppressWarnings("deprecation")
     public static void onRenderArm(RenderArmEvent event) {
-        CuriosApi.getCuriosHelper().getCuriosHandler(event.getPlayer()).ifPresent(handler -> {
-            ICurioStacksHandler stacksHandler = handler.getCurios().get(SlotTypePreset.HANDS.getIdentifier());
+        CuriosApi.getCuriosInventory(event.getPlayer()).ifPresent(handler -> {
+            ICurioStacksHandler stacksHandler = handler.getCurios().get(ExpandedCombat.GAUNTLET_CURIOS_IDENTIFIER);
             if (stacksHandler != null) {
                 IDynamicStackHandler stacks = stacksHandler.getStacks();
                 IDynamicStackHandler cosmeticStacks = stacksHandler.getCosmeticStacks();
                 ItemStack stack = cosmeticStacks.getStackInSlot(0);
-                boolean cosmetic = true;
+
                 if (stack.isEmpty() && stacksHandler.getRenders().get(0)) {
                     stack = stacks.getStackInSlot(0);
-                    cosmetic = false;
                 }
-                if (MinecraftForge.EVENT_BUS.post(new GauntletRenderFirstPersonEvent(event.getPoseStack(), event.getMultiBufferSource(), event.getPackedLight(), event.getPlayer(), event.getArm(), stack, cosmetic))) return;
 
                 if (stack.getItem() instanceof GauntletItem gauntletItem) {
                     ICurioRenderer iCurioRenderer = gauntletItem.getGauntletRenderer().get();
@@ -125,95 +122,5 @@ public class GauntletEvents
                 }
             }
         });
-    }
-
-
-
-    /**
-     * Fired before the player's gauntlet is rendered in first person. This is a more targeted version of {@link RenderArmEvent},
-     * and can be used to replace the rendering of the player's gauntlet.
-     *
-     * <p>This event is {@linkplain Cancelable cancellable}, and does not {@linkplain HasResult have a result}.
-     * If this event is cancelled, then the gauntlet will not be rendered, however unlike in {@link RenderArmEvent} the arm will still render.</p>
-     *
-     * <p>This event is fired on the {@linkplain MinecraftForge#EVENT_BUS main Forge event bus},
-     * only on the {@linkplain LogicalSide#CLIENT logical client}.</p>
-     */
-    @Cancelable
-    public static class GauntletRenderFirstPersonEvent extends Event {
-        private final PoseStack poseStack;
-        private final MultiBufferSource multiBufferSource;
-        private final int packedLight;
-        private final AbstractClientPlayer player;
-        private final HumanoidArm arm;
-        private final ItemStack gauntlet;
-        private final boolean cosmetic;
-
-        @ApiStatus.Internal
-        public GauntletRenderFirstPersonEvent(PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight, AbstractClientPlayer player, HumanoidArm arm, ItemStack gauntlet, boolean cosmetic)
-        {
-            this.poseStack = poseStack;
-            this.multiBufferSource = multiBufferSource;
-            this.packedLight = packedLight;
-            this.player = player;
-            this.arm = arm;
-            this.gauntlet = gauntlet;
-            this.cosmetic = cosmetic;
-        }
-
-        /**
-         * {@return the arm being rendered}
-         */
-        public HumanoidArm getArm()
-        {
-            return arm;
-        }
-
-        /**
-         * {@return the pose stack used for rendering}
-         */
-        public PoseStack getPoseStack()
-        {
-            return poseStack;
-        }
-
-        /**
-         * {@return the source of rendering buffers}
-         */
-        public MultiBufferSource getMultiBufferSource()
-        {
-            return multiBufferSource;
-        }
-
-        /**
-         * {@return the amount of packed (sky and block) light for rendering}
-         *
-         * @see LightTexture
-         */
-        public int getPackedLight()
-        {
-            return packedLight;
-        }
-
-        /**
-         * {@return the client player that is having their arm rendered} In general, this will be the same as
-         * {@link net.minecraft.client.Minecraft#player}.
-         */
-        public AbstractClientPlayer getPlayer()
-        {
-            return player;
-        }
-        /**
-         * {@return the gauntlet ItemStack being rendering}
-         */
-        public ItemStack getGauntlet() {
-            return gauntlet;
-        }
-        /**
-         * {@return weather the gauntlet ItemStack being rendering is cosmetic or not}
-         */
-        public boolean isCosmetic() {
-            return cosmetic;
-        }
     }
 }
