23c6e68f9d0a8097556fb9fec668da6bb6220a9d
userofbricks <userofbricks11@gmail.com>
2024-06-14 18:56:55 -0600

    updated GeneralModEventBusEvents

--- src/main/java/com/userofbricks/expanded_combat/events/GeneralModEventBusEvents.java
+++ src/main/java/com/userofbricks/expanded_combat/events/GeneralModEventBusEvents.java
@@ -3,57 +3,31 @@ package com.userofbricks.expanded_combat.events;
 import com.userofbricks.expanded_combat.ExpandedCombat;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraftforge.common.Tags;
-import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-
-import java.util.Arrays;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityAttributeModificationEvent;
 
 import static com.userofbricks.expanded_combat.init.ECAttributes.*;
 
-@Mod.EventBusSubscriber(modid = ExpandedCombat.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = ExpandedCombat.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class GeneralModEventBusEvents {
     @SubscribeEvent
     public static void existingEntityAttributes(EntityAttributeModificationEvent event) {
-        for (EntityType<? extends LivingEntity> entityType : event.getTypes() /*Arrays.asList(
-                EntityType.ALLAY,
-                EntityType.DROWNED,
-                EntityType.ENDERMAN,
-                EntityType.EVOKER,
-                EntityType.GIANT,
-                EntityType.HUSK,
-                EntityType.ILLUSIONER,
-                EntityType.PIGLIN,
-                EntityType.PIGLIN_BRUTE,
-                EntityType.SKELETON,
-                EntityType.STRAY,
-                EntityType.VEX,
-                EntityType.VILLAGER,
-                EntityType.VINDICATOR,
-                EntityType.WANDERING_TRADER,
-                EntityType.WARDEN,
-                EntityType.WITCH,
-                EntityType.WITHER_SKELETON,
-                EntityType.ZOMBIE,
-                EntityType.ZOMBIE_VILLAGER,
-                EntityType.ZOMBIFIED_PIGLIN,
-                EntityType.PLAYER
-        )*/) {
-            if (!event.has(entityType, GAUNTLET_DMG_WITHOUT_WEAPON.get())) {
-                event.add(entityType, GAUNTLET_DMG_WITHOUT_WEAPON.get());
+        for (EntityType<? extends LivingEntity> entityType : event.getTypes()) {
+            if (!event.has(entityType, GAUNTLET_DMG_WITHOUT_WEAPON)) {
+                event.add(entityType, GAUNTLET_DMG_WITHOUT_WEAPON);
             }
-            if (!event.has(entityType, HEAT_DMG.get())) {
-                event.add(entityType, HEAT_DMG.get());
+            if (!event.has(entityType, HEAT_DMG)) {
+                event.add(entityType, HEAT_DMG);
             }
-            if (!event.has(entityType, COLD_DMG.get())) {
-                event.add(entityType, COLD_DMG.get());
+            if (!event.has(entityType, COLD_DMG)) {
+                event.add(entityType, COLD_DMG);
             }
-            if (!event.has(entityType, VOID_DMG.get())) {
-                event.add(entityType, VOID_DMG.get());
+            if (!event.has(entityType, VOID_DMG)) {
+                event.add(entityType, VOID_DMG);
             }
-            if (!event.has(entityType, SOUL_DMG.get())) {
-                event.add(entityType, SOUL_DMG.get());
+            if (!event.has(entityType, SOUL_DMG)) {
+                event.add(entityType, SOUL_DMG);
             }
         }
     }
