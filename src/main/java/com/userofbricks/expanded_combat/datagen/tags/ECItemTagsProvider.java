10e0081df5b358cf07614c37563ebca02331afe8
userofbricks <userofbricks11@gmail.com>
2024-10-24 22:12:37 -0700

    added enchantable tag datagen

--- src/main/java/com/userofbricks/expanded_combat/datagen/tags/ECItemTagsProvider.java
+++ src/main/java/com/userofbricks/expanded_combat/datagen/tags/ECItemTagsProvider.java
@@ -1,12 +1,15 @@
 package com.userofbricks.expanded_combat.datagen.tags;
 
-import com.userofbricks.expanded_combat.init.ECTags;
 import com.userofbricks.expanded_combat.init.ECItems;
+import com.userofbricks.expanded_combat.init.ECTags;
 import com.userofbricks.expanded_combat.item.*;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.Tags;
@@ -29,26 +32,44 @@ public class ECItemTagsProvider extends ItemTagsProvider {
         for (DeferredHolder<Item, ? extends Item> deferredItem : ECItems.ITEMS.getEntries()) {
             if (deferredItem.get() instanceof ECArrowItem item) {
                 tag(ItemTags.ARROWS).add(item);
-            } else if (deferredItem.get() instanceof ECBowItem item) {
+            }
+            if (deferredItem.get() instanceof ECBowItem item) {
                 tag(Tags.Items.TOOLS_BOW).add(item);
-            } else if (deferredItem.get() instanceof ECCrossBowItem item) {
+            }
+            if (deferredItem.get() instanceof ECCrossBowItem item) {
                 tag(Tags.Items.TOOLS_CROSSBOW).add(item);
-            } else if (deferredItem.get() instanceof GauntletItem item) {
+            }
+            if (deferredItem.get() instanceof GauntletItem item) {
                 tag(ECTags.GAUNTLETS).add(item);
                 tag(ItemTags.TRIMMABLE_ARMOR).add(item);
-            } else if (deferredItem.get() instanceof QuiverItem item) {
+                tag(ECTags.GAUNTLET_ENCHANTABLE).add(item);
+                tag(ECTags.AGILITY_ENCHANTABLE).add(item);
+            }
+            if (deferredItem.get() instanceof QuiverItem item) {
                 tag(ECTags.QUIVERS).add(item);
-            } else if (deferredItem.get() instanceof PotionWeaponItem item) {
+            }
+            if (deferredItem.get() instanceof PotionWeaponItem item) {
                 tag(ECTags.POTION_WEAPONS).add(item);
-            } else if (deferredItem.get() instanceof ECWeaponItem item && (
+            }
+            if (deferredItem.get() instanceof ECWeaponItem item && (
                     item.weapon.id() == BATTLE_STAFF.id()
                             || item.weapon.id() == BROAD_SWORD.id()
                             || item.weapon.id() == CLAYMORE.id()
                             || item.weapon.id() == DANCERS_SWORD.id()
                             || item.weapon.id() == GLAIVE.id()
-                            || item.weapon.id() == SCYTHE.id()
                     )) {
                 tag(ItemTags.DYEABLE).add(item);
+            }
+            if (deferredItem.get() instanceof ArrowBlockWeaponItem item) {
+                tag(ECTags.BLOCKING_ENCHANTABLE).add(item);
+            }
+            if (deferredItem.get() instanceof SlamWeaponItem item) {
+                tag(ECTags.GROUND_SLAM).add(item);
+            }
+        }
+        for (Item item :BuiltInRegistries.ITEM.stream().toList()) {
+            if (item instanceof ArmorItem armorItem && (armorItem.getEquipmentSlot() == EquipmentSlot.FEET || armorItem.getEquipmentSlot() == EquipmentSlot.LEGS || armorItem.getEquipmentSlot() == EquipmentSlot.CHEST)) {
+                tag(ECTags.AGILITY_ENCHANTABLE).add(item);
             }
         }
 
